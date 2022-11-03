00cc74ddb33431dc32f30e6836b04cd4da04a4f9
Coolsnake8 <foxtailrb@gmail.com>
2022-11-02 23:31:21 -0600

    Populated the database with fake data.

---
+++ ToDoList/Services/Database.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public class Database
+    {
+        public IEnumerable<TodoItem> GetItems() => new[]
+        {
+            new TodoItem {Description = "Walk the dog."},
+            new TodoItem {Description = "Buy some milk."},
+            new TodoItem {Description = "Learn Avalonia", IsChecked = true},
+        };
+    }
+}
+
