d0999ffa739cb5129880348e9ba3db17e63ec989
Coolsnake8 <foxtailrb@gmail.com>
2022-11-02 23:35:41 -0600

    Setup the file for the "View Model".

---
+++ ToDoList/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public class TodoListViewModel : ViewModelBase
+    {
+        public TodoListViewModel(IEnumerable<TodoItem> items)
+        {
+            Items = new ObservableCollection<TodoItem>(items)
+        }
+        
+        public ObservableCollection<TodoItem> Items { get; }
+    }
+}
+
